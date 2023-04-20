df23d7783d7ff208607fd64b9f5b3129ea9cb7e0
Prissilla Escobar <prissilla988@gmail.com>
2023-04-20 12:24:24 -0600

    Completed clean up of all four examples

--- cleanUp.js
+++ cleanUp.js
@@ -8,41 +8,45 @@
 
 
 /* EX 1: I moved the console log to line 11, entered the curly brackets with where they belong on
-on lines 11 and 15, indented the console log. */
+on lines 11 and 15, indented the console log. Semi colons on lines 13 & 17 */
 function askForName() {
   console.log("Hello, what is your name?");
-
 }
 
 askForName();
 
 
-// EX 2:
+/* EX 2: I indented lines 22, created an extra line between line 22 and the return,
+removed the indentations from line 26 in front of the curly brackets, print code */
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+  var sum = first + second + third;
+  return sum;
+}
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
 
-// EX 3:
-func makeFreshPesto(){
+/* EX 3: add a space between parenthesis and curly bracket, move close curly bracket down
+and remove indent, spell out function */
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");
+}
 
 makeFreshPesto();
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+/*  EX 4: move open curly bracket to line 44, indent line 45, remove one indent from line 46,
+move return to line 48 and indent, closing curly bracket on line 49, semi colon on line 48, wrote
+the print for fun   */
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return avg;
+}
 
-return avg
-  }
+console.log(average(10,12))
