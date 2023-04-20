8852d5d8075fcb53a302a421a9a6b8f05c237774
Prissilla Escobar <prissilla988@gmail.com>
2023-04-20 15:16:16 -0600

    write functions for examples 3-5

--- functions.js
+++ functions.js
@@ -30,14 +30,43 @@ console.log(salute)
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+function greetPerson(first, middle, last) {
+    return `What it do, ${first} ${middle} ${last}?!`
+}
 
+var greet = greetPerson("Lex", "Greyson", "Escobar")
+var sayHi = greetPerson("Frank", "Arthur", "Escobar")
+
+console.log(greet)
+console.log(sayHi)
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
+function square(num1) {
+    return num1*num1
+}
+
+var lex = square(5)
+var rag = square(98)
+
+console.log(`5 squared is ${lex}`)
+console.log(`98 sqared is ${rag}`)
 
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
+
+function checkStock(num, item) {
+    if (num >= 4) {
+        console.log(`${item} is stocked!`)
+    } else if (num > 0) {
+        console.log(`${item} - running LOW!`)
+    } else {
+        console.log(`${item} - OUT OF STOCK!`)
+    }
+}
+
+
 
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
